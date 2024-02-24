ae18e5b6ad6b9c706ff427a0447d57290afbb8dc
Toshish Dhote <825toshish@gmail.com>
2024-02-24 11:00:56 +0530

    added new Comparison Across Locations

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import plotly.express as px
+import numpy as np
 def load_data(year, type):
     data = pd.read_excel(f"xlsxFiles/{type}/{year}.xlsx")
     dataFrame = getDataFrame(data)
@@ -12,3 +13,53 @@ def getDataFrame(excleData):
     pd.set_option('display.max_rows', None)
     dataFrame = excleData
     return dataFrame
+def getcomparisonAcrossLocationData():
+    data = pd.read_excel(f"xlsxFiles/air_pollution_dataset.xlsx")
+        # Replace '-' with NaN
+    data.replace('-', np.nan, inplace=True)
+
+    # Convert non-finite values to NaN
+    data = data.replace([np.inf, -np.inf], np.nan)
+
+# Convert float columns to integers
+    float_columns = data.select_dtypes(include=[np.float64]).columns
+    data[float_columns] = data[float_columns].fillna(0).astype(int)
+    return data
+def comparisonAcrossLocationsSO2():
+    data = getcomparisonAcrossLocationData()
+
+    so2_data = data[['State/UT', 'Annual Average SO2']].copy()
+    location_so2_mean = so2_data.groupby('State/UT')['Annual Average SO2'].mean().sort_values().reset_index()
+
+    fig = px.bar(location_so2_mean, x='Annual Average SO2', y='State/UT', orientation='h',
+                title='Mean SO2 Levels Across Different States/UT',
+                labels={'Annual Average SO2': 'Mean SO2 Levels', 'State/UT': 'State/UT'})
+    
+    return fig.to_html(full_html=False, include_plotlyjs='cdn', default_height=500)
+
+
+def comparisonAcrossLocationsNO2():
+    data = getcomparisonAcrossLocationData()
+    no2_data = data[['State/UT', 'Annual Average NO2']].copy()
+
+    # Group data by location and calculate mean NO2 levels
+    location_no2_mean = no2_data.groupby('State/UT')['Annual Average NO2'].mean().sort_values().reset_index()
+
+    # Plotting
+    fig = px.bar(location_no2_mean, x='Annual Average NO2', y='State/UT', orientation='h',
+                title='Mean NO2 Levels Across Different States/UT',
+                labels={'Annual Average NO2': 'Mean NO2 Levels', 'State/UT': 'State/UT'})
+    return fig.to_html(full_html=False, include_plotlyjs='cdn', default_height=500)
+
+def comparisonAcrossLocationsPM10():
+    data = getcomparisonAcrossLocationData()
+    pm10_data = data[['State/UT', 'Annual Average PM10']].copy()
+
+    # Group data by location and calculate mean PM10 levels
+    location_pm10_mean = pm10_data.groupby('State/UT')['Annual Average PM10'].mean().sort_values().reset_index()
+
+    # Plotting
+    fig = px.bar(location_pm10_mean, x='Annual Average PM10', y='State/UT', orientation='h',
+                title='Mean PM10 Levels Across Different States/UT',
+                labels={'Annual Average PM10': 'Mean PM10 Levels', 'State/UT': 'State/UT'})
+    return fig.to_html(full_html=False, include_plotlyjs='cdn', default_height=500)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from data_processing import load_data
+from data_processing import load_data, comparisonAcrossLocationsSO2, comparisonAcrossLocationsNO2, comparisonAcrossLocationsPM10
 import pandas as pd
 app = Flask(__name__, static_folder='static')
 
@@ -52,6 +52,27 @@ def noise_dataset_year(year,type):
     data = load_data(year=year, type=type)
     #print(data)
     return render_template("noiseDataSetYearList.html", data=data)
-    
+
+@app.route("/insights")
+def insights():
+    return render_template("insights.html")
+
+@app.route("/comparisonAcrossLocationsSO2")
+def CALSO2():
+    data = comparisonAcrossLocationsSO2()
+    return render_template("CAL_SO2.html", data = data)
+
+@app.route("/comparisonAcrossLocationsNO2")
+def CALNO2():
+    data = comparisonAcrossLocationsNO2()
+    return render_template("CAL_NO2.html", data = data)
+
+
+@app.route("/comparisonAcrossLocationsPM10")
+def CALPM10():
+    data = comparisonAcrossLocationsPM10()
+    return render_template("CAL_PM10.html", data = data)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
