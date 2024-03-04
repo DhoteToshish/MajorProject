1df2122cbbf076636f621b8c8756b79bbef60b0d
Toshish Dhote <825toshish@gmail.com>
2024-03-04 22:16:00 +0530

    added plotly for visualization

--- data_processing.py
+++ data_processing.py
@@ -15,13 +15,13 @@ def getDataFrame(excleData):
     return dataFrame
 def getcomparisonAcrossLocationData():
     data = pd.read_excel(f"xlsxFiles/air_pollution_dataset.xlsx")
-        # Replace '-' with NaN
+        
     data.replace('-', np.nan, inplace=True)
 
-    # Convert non-finite values to NaN
+    
     data = data.replace([np.inf, -np.inf], np.nan)
 
-# Convert float columns to integers
+    
     float_columns = data.select_dtypes(include=[np.float64]).columns
     data[float_columns] = data[float_columns].fillna(0).astype(int)
     return data
@@ -42,10 +42,10 @@ def comparisonAcrossLocationsNO2():
     data = getcomparisonAcrossLocationData()
     no2_data = data[['State/UT', 'Annual Average NO2']].copy()
 
-    # Group data by location and calculate mean NO2 levels
+    
     location_no2_mean = no2_data.groupby('State/UT')['Annual Average NO2'].mean().sort_values().reset_index()
 
-    # Plotting
+    
     fig = px.bar(location_no2_mean, x='Annual Average NO2', y='State/UT', orientation='h',
                 title='Mean NO2 Levels Across Different States/UT',
                 labels={'Annual Average NO2': 'Mean NO2 Levels', 'State/UT': 'State/UT'})
@@ -55,10 +55,10 @@ def comparisonAcrossLocationsPM10():
     data = getcomparisonAcrossLocationData()
     pm10_data = data[['State/UT', 'Annual Average PM10']].copy()
 
-    # Group data by location and calculate mean PM10 levels
+    
     location_pm10_mean = pm10_data.groupby('State/UT')['Annual Average PM10'].mean().sort_values().reset_index()
 
-    # Plotting
+   
     fig = px.bar(location_pm10_mean, x='Annual Average PM10', y='State/UT', orientation='h',
                 title='Mean PM10 Levels Across Different States/UT',
                 labels={'Annual Average PM10': 'Mean PM10 Levels', 'State/UT': 'State/UT'})
